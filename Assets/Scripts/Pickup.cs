924cf7bbc933a0be4c8fc6df2ff83fd37d3fb736
Chris Karpyszyn <ckarpyszyn@tractionrec.com>
2022-02-21 15:08:22 -0500

    create pickup prefab with particle effect and script

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public Transform pickupEffect;
+    private int value = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Transform effect = Instantiate(pickupEffect, transform.position, transform.rotation);
+            Destroy(effect.gameObject, 3);
+            Destroy(gameObject);
+        }
+    }
+}
